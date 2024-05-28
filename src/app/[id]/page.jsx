cdca898ba3a6014d0346385374f5d6aed88af19e
BernardoCameron <b.cameron.i26@gmail.com>
2024-05-28 16:21:50 -0400

    Agregando caracteristicas al detalle de cancha

--- src/app/[id]/page.jsx
+++ src/app/[id]/page.jsx
@@ -9,6 +9,8 @@ import Image from "next/image";
 import { useParams } from 'next/navigation';
 import { useEffect, useState } from 'react';
 import { useTheme } from '@/context/ThemeContext';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCheckCircle, faTimesCircle } from '@fortawesome/free-solid-svg-icons';
 
 export default function Page() {
   const { id } = useParams();
@@ -29,6 +31,7 @@ export default function Page() {
           Tamanio,
           Precio_hora,
           Disciplina_id,
+          Caracteristicas,
           Imagen_cancha (
             Url_img
           )
@@ -51,40 +54,68 @@ export default function Page() {
     }
   }, [id]);
 
-  const { Nombre, Precio_hora, imagen, Superficie, Tamanio } = cancha;
+  console.log(cancha.Caracteristicas);
+
+  const { Nombre, Precio_hora, imagen, Superficie, Tamanio, Caracteristicas } = cancha;
 
   return (
     <>
       <Navbar />
-      <div className='mt-32 flex items-center mx-auto w-11/12'>
-        <Link href='/'>
-          <Button variant='outline' className={theme === 'dark' ? 'bg-gray-700 text-white' : ''}>Volver</Button>
-        </Link>
-      </div>
-      <main className="flex items-center justify-center">
-        <div className={`flex flex-col md:flex-row items-center ${theme === 'dark' ? 'bg-gray-800 text-white border-gray-700' : 'bg-white text-black border-gray-200'} rounded-lg shadow my-20 md:w-11/12 max-w-[1100px] mx-6`}>
-          {imagen && (
+      <main className={`flex flex-col items-center ${theme === 'dark' ? 'bg-gray-800 text-white' : 'bg-white text-black'} min-h-screen p-6`}>
+        <div className='mt-32 flex items-center mx-auto w-11/12'>
+          <Link href='/'>
+            <Button variant='outline' className={theme === 'dark' ? 'bg-gray-700 text-white' : ''}>Volver</Button>
+          </Link>
+        </div>
+        <h1 className={`mb-4 text-4xl font-bold ${theme === 'dark' ? 'text-white' : 'text-gray-900'}`}>
+          {Nombre}
+        </h1>
+
+        {imagen && (
+          <div className="w-full max-w-4xl mb-6">
             <Image
-              className="flex-1 object-cover w-full rounded-t-lg h-96 md:h-80 md:w-48 md:rounded-none md:rounded-s-lg"
+              className="object-cover w-full rounded-lg"
               src={imagen}
-              width={600}  // Ajusta estos valores según sea necesario
-              height={400}  // Ajusta estos valores según sea necesario
+              width={1200}  // Ajusta estos valores según sea necesario
+              height={800}  // Ajusta estos valores según sea necesario
               priority
               alt="imagen de la cancha"
             />
-          )}
-          
-          <div className="flex flex-col justify-between p-4 leading-normal flex-1 min-h-72">
-            <h5 className={`mb-2 text-2xl font-bold tracking-tight ${theme === 'dark' ? 'text-white' : 'text-gray-900'}`}>
-              {Nombre}
-            </h5>
-            <ul className={`mb-3 font-normal ${theme === 'dark' ? 'text-gray-300' : 'text-gray-700'}`}>
-                <li>{Superficie}, {Tamanio}</li>
-                
-                <li className='font-semibold text-lg mt-3'>$ {Precio_hora}</li>
-            </ul>
-            <Button>Reservar</Button>
           </div>
+        )}
+
+        <div className={`w-full max-w-4xl ${theme === 'dark' ? 'bg-gray-900 text-white border-gray-700' : 'bg-gray-100 text-black border-gray-200'} rounded-lg shadow p-6`}>
+          <h6 className={`mb-4 text-center text-xl font-medium ${theme === 'dark' ? 'text-gray-400' : 'text-gray-800'}`}>Características</h6>
+
+          <table className={`w-full mb-6 ${theme === 'dark' ? 'text-gray-300' : 'text-gray-700'}`}>
+            <thead>
+              <tr>
+                <th className="border px-4 py-2">Característica</th>
+                <th className="border px-4 py-2">Disponibilidad</th>
+              </tr>
+            </thead>
+            <tbody>
+              {Caracteristicas && Object.keys(Caracteristicas).map((key) => (
+                <tr key={key}>
+                  <td className="border px-4 py-2">{key}</td>
+                  <td className="border px-4 py-2 text-center">
+                    {Caracteristicas[key] === 'True' ? (
+                      <FontAwesomeIcon icon={faCheckCircle} className="text-green-500" />
+                    ) : (
+                      <FontAwesomeIcon icon={faTimesCircle} className="text-red-500" />
+                    )}
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+
+          <ul className={`mb-6 font-normal ${theme === 'dark' ? 'text-gray-300' : 'text-gray-700'}`}>
+            <li>{Superficie}, {Tamanio}</li>
+            <li className='font-semibold text-lg mt-3'>$ {Precio_hora}</li>
+          </ul>
+
+          <Button className='w-full'>Reservar</Button>
         </div>
       </main>
       <Footer />
