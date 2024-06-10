c05e89142a98a0d04d7467ce3f6e514d7590432a
caZeladaSgit <cazelada.s@gmail.com>
2024-06-10 01:11:32 -0400

    Policies actualizada con deportes

--- src/app/policies/page.jsx
+++ src/app/policies/page.jsx
@@ -2,53 +2,48 @@
 import React from 'react';
 import { useTheme } from '@/context/ThemeContext';
 import Image from 'next/image';
+import Link from 'next/link';
 
-// Cambiar el nombre de la función a 'Page' para seguir la convención de los componentes de React
+
 export default function Page() {
   const { theme } = useTheme();
 
   return (
-    <section className={`w-full flex flex-col items-center mt-10 pt-20 pb-10 transition-colors duration-500 ${theme === 'dark' ? 'bg-gray-800 text-white' : 'bg-[#F4F4F4] text-black'}`}>
-      <div className='flex flex-col md:flex-row justify-evenly w-full max-w-screen-xl px-4 md:px-8'>
-        {/* Contenedor del bloque de políticas */}
-        <div className='flex flex-col gap-8 justify-center w-full md:w-2/3'>
-          {/* Título del bloque */}
-          <h1 className='text-5xl font-bold underline mb-4'>Políticas del Producto</h1>
-          <div className='grid grid-cols-1 md:grid-cols-2 gap-8'>
-            {/* Políticas distribuidas en columnas */}
-            <div className='p-4 bg-white shadow-md rounded-md transform transition-transform hover:scale-105 hover:shadow-lg'>
-              <h2 className='text-2xl font-semibold mb-2'>Reservas</h2>
-              <p>Puedes reservar una cancha con hasta 30 días de antelación. Las reservas deben ser confirmadas al menos 24 horas antes del tiempo de juego.</p>
-            </div>
-            <div className='p-4 bg-white shadow-md rounded-md transform transition-transform hover:scale-105 hover:shadow-lg'>
-              <h2 className='text-2xl font-semibold mb-2'>Cancelaciones</h2>
-              <p>Las cancelaciones deben realizarse al menos 48 horas antes del tiempo de juego para obtener un reembolso completo. Las cancelaciones dentro de las 48 horas tendrán una penalización del 50%.</p>
-            </div>
-            <div className='p-4 bg-white shadow-md rounded-md transform transition-transform hover:scale-105 hover:shadow-lg'>
-              <h2 className='text-2xl font-semibold mb-2'>Puntualidad</h2>
-              <p>Te pedimos que llegues a tiempo para tu reserva. Si llegas tarde, el tiempo perdido no será reembolsado.</p>
-            </div>
-            <div className='p-4 bg-white shadow-md rounded-md transform transition-transform hover:scale-105 hover:shadow-lg'>
-              <h2 className='text-2xl font-semibold mb-2'>Responsabilidad</h2>
-              <p>Los usuarios son responsables de cualquier daño causado a las instalaciones durante el tiempo de su reserva.</p>
-            </div>
-            <div className='p-4 bg-white shadow-md rounded-md transform transition-transform hover:scale-105 hover:shadow-lg'>
-              <h2 className='text-2xl font-semibold mb-2'>Condiciones Climáticas</h2>
-              <p>En caso de mal tiempo, podrás reprogramar tu reserva sin penalización.</p>
+    <section className={`w-full flex flex-col items-center justify-center mt-10 pt-20 pb-10 transition-colors duration-500 ${theme === 'dark' ? 'bg-gray-800 text-white' : 'bg-[#F4F4F4] text-black'}`}>
+      <div className='w-full max-w-screen-xl'>
+        <div className='flex flex-col md:flex-row justify-center'>
+          <div className='flex flex-col gap-8 justify-center w-full md:w-2/3'>
+            <h1 className='text-5xl font-bold underline mb-4 text-center'>Políticas Generales</h1>
+            <div className='grid grid-cols-1 md:grid-cols-2 gap-8'>
+              <div className='p-4 bg-white shadow-md rounded-md transform transition-transform hover:scale-105 hover:shadow-lg flex items-center justify-center'>
+              <Link href="/policies/sport/futbol">
+                <h2 className='text-2xl font-semibold mb-2'>Futbol</h2>
+              </Link>
+              </div>
+              <div className='p-4 bg-white shadow-md rounded-md transform transition-transform hover:scale-105 hover:shadow-lg flex items-center justify-center'>
+              <Link href="/policies/sport/baloncesto">
+                <h2 className='text-2xl font-semibold mb-2'>Baloncesto</h2>
+              </Link>
+              </div>
+              <div className='p-4 bg-white shadow-md rounded-md transform transition-transform hover:scale-105 hover:shadow-lg flex items-center justify-center'>
+              <Link href="/policies/sport/padel">
+                <h2 className='text-2xl font-semibold mb-2'>Pádel</h2>
+              </Link>
+              </div>
+              <div className='p-4 bg-white shadow-md rounded-md transform transition-transform hover:scale-105 hover:shadow-lg flex items-center justify-center'>
+              <Link href="/policies/sport/tenis">
+                <h2 className='text-2xl font-semibold mb-2'>Tenis</h2>
+              </Link>
+              </div>
             </div>
           </div>
         </div>
-        {/* Imagen del hero */}
-        <div className='hidden md:block rounded-md shadow-xl md:w-1/3'>
-          <Image 
-            className='rounded-md shadow-xl'
-            src={'https://images.unsplash.com/photo-1579952363873-27f3bade9f55?q=80&w=3072&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'}
-            width={370} 
-            height={370}
-            alt='Imagen de canchas deportivas'
-          />
-        </div>
       </div>
+      <Link href="/">
+        <button className="inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border border-input bg-background hover:bg-accent hover:text-accent-foreground h-10 px-4 py-2 mt-8">
+          Volver al Home
+        </button>
+      </Link>
     </section>
   );
 }
